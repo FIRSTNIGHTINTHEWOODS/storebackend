7ec77d721d1719ebb0f1972e5da63929c363727c
firstnightinthewoods <vyacheslav2310zh@gmail.com>
2019-01-29 00:48:37 +0200

    init commit 2

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,28 @@ import express from "express";
 import * as bodyParser from "body-parser"; // used to parse the form data that you pass in the request
 import routes from "./routes";
 import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+import mongo from "connect-mongo";
+import mongoose from "mongoose";
+import bluebird from "bluebird";
+
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+
+const MongoStore = mongo(session);
+
+// Load environment variables from .env file, where API keys and passwords are configured
+dotenv.config({ path: ".env" });
+
+// Connect to MongoDB
+const mongoUrl = MONGODB_URI;
+(<any>mongoose).Promise = bluebird;
+mongoose.connect(mongoUrl, { useNewUrlParser: true }).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+).catch(err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    // process.exit();
+});
 
 class App {
   public app: express.Application;
@@ -12,7 +34,6 @@ class App {
   }
 
   private config(): void {
-    
      this.app.use(cors());
     // support application/json type post data
     this.app.use(bodyParser.json());
@@ -22,6 +43,16 @@ class App {
         extended: false
       })
     );
+
+    this.app.use(session({
+        resave: true,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        store: new MongoStore({
+            url: mongoUrl,
+            autoReconnect: true
+        })
+    }));
 
     this.app.use("/api/v1", routes);
   }
